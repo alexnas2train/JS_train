ce797083252e2c4a521f01f95192ac28a72c949a
Alex <aaa@aaa.aaa>
2018-12-16 10:44:12 +0530

    This is our first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+'use strict';
+// alert(second);
+var leftBorderWidth = 1; {
+    let second = 2;
+}
+const pi = 3.14;
+
+// alert(second);
+
+// console.log('10' / 0);
+console.log('string' / 0);
+
+let something = null;
+console.log(something);
+let person = {
+    name: 'John',
+    isMarried: false
+};
+console.log(person.name, person['isMarried']);
+let arr = ['plum.png', 'orange.jpg', ];
